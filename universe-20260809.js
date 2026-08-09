3bd34ac0996c3783a61b2517ef05f48fde841a51
IsmaelS <114076344+SISA0729@users.noreply.github.com>
2026-08-09 09:30:54 +0200

    Baja el volumen de la música al 40%

--- universe-20260809.js
+++ universe-20260809.js
@@ -8,7 +8,7 @@ const backgroundMusic = document.querySelector('#backgroundMusic');
 const musicHeart = document.querySelector('#musicHeart');
 
 function playBackgroundMusic() {
-  backgroundMusic.volume = .65;
+  backgroundMusic.volume = 0.4;
   backgroundMusic.play().then(() => {
     musicHeart.classList.add('music-playing');
     musicHeart.setAttribute('aria-label', 'Música reproduciéndose');
